5169da148327761f70a92262257b9835cd97283b
DenuccioDiego <diegodenuccio95@gmail.com>
2021-10-13 16:17:59 +0200

    Condizioni nel ciclo (al momento solamente utilizzando console.log

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -12,12 +12,23 @@ const sec = document.querySelector("div.container")
 
 
 
-for (let i = 0; i < 100; i++){
+for (let i = 1; i < 101; i++){
      
      // numero = i;
 
      // selezioneDiv.append(quadrato);
      // quadrato.append(numero)
+     if ((i % 3 == 0) && (i % 5  == 0)){
+          console.log("ciao 5 and 3")
+     }
+
+     else if (i % 3 == 0) {
+          console.log("ciao 3");
+     }
+
+     else if (i % 5  == 0) {
+          console.log("ciao 5")
+     }
 
      const element = `<div class="box">${i}</div>`;
      sec.insertAdjacentHTML('beforeend', element)
