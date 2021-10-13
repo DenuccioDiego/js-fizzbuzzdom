c159fe2f01b8f591ce3efcb831877765494c0cc5
DenuccioDiego <diegodenuccio95@gmail.com>
2021-10-13 17:01:46 +0200

    Esercizio completo (non so che commenti aggiunegere)

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -1,32 +1,28 @@
 
+const container_principale = document.querySelector("div.container")
 
-const sec = document.querySelector("div.container")
-
-
-for (let i = 1; i < 101; i++){
+for (let i = 1; i < 101; i++) {
      
      if (!((i % 3 == 0) || (i % 5  == 0))) {
-          const element = `<div class="box standard_color">${i}</div>`;
-          sec.insertAdjacentHTML('beforeend', element)
+          const boxColorato = `<div class="box standard_color">${i}</div>`;
+          container_principale.insertAdjacentHTML('beforeend', boxColorato);
      }
 
-     else if ((i % 3 == 0) && (i % 5  == 0)){
-          const element = `<div class="box fizz_buzz_color">fizz buzz</div>`;
-          sec.insertAdjacentHTML('beforeend', element)
-          }
+     else if ((i % 3 == 0) && (i % 5  == 0)) {
+          const boxColorato = `<div class="box fizz_buzz_color">fizz buzz</div>`;
+          container_principale.insertAdjacentHTML('beforeend', boxColorato);
+     }
 
      else if (i % 3 == 0) {
-          const element = `<div class="box fizz_color">fizz</div>`;
-          sec.insertAdjacentHTML('beforeend', element)
-          
+          const boxColorato = `<div class="box fizz_color">fizz</div>`;
+          container_principale.insertAdjacentHTML('beforeend', boxColorato);   
      }
 
      else if (i % 5  == 0) {
-          const element = `<div class="box buzz_color">buzz</div>`;
-          sec.insertAdjacentHTML('beforeend', element)
+          const boxColorato = `<div class="box buzz_color">buzz</div>`;
+          container_principale.insertAdjacentHTML('beforeend', boxColorato);
      }
 
-    
 }
 
 
