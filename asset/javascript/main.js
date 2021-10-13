7f8960fce3edcc4e634fa96c06615405e62dd1ff
DenuccioDiego <diegodenuccio95@gmail.com>
2021-10-13 15:56:07 +0200

    creazione struttura For senza IF (quindi blocchi tutti uguali)

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -1,6 +1,27 @@
 
 
+// let numero = 0 ;
 
+// let selezioneDiv = document.querySelector("div.container")
+// let quadrato = document.createElement("div.box") ;
+
+const sec = document.querySelector("div.container")
+
+
+
+
+
+
+for (let i = 0; i < 100; i++){
+     
+     // numero = i;
+
+     // selezioneDiv.append(quadrato);
+     // quadrato.append(numero)
+
+     const element = `<div class="box">${i}</div>`;
+     sec.insertAdjacentHTML('beforeend', element)
+}
 
 
 
