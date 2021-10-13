29be7344fd0f4d9a8300dac528e1365787318de2
DenuccioDiego <diegodenuccio95@gmail.com>
2021-10-13 17:25:31 +0200

    ESERCIZIO COMPLETO(con commenti)

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -1,26 +1,36 @@
-
+//Dichiarazione variabile selezionando il contenitore principale 
 const container_principale = document.querySelector("div.container")
 
+
+//Imposto le condizini per il ciclo . Creazione box colorati.
 for (let i = 1; i < 101; i++) {
      
-     if (!((i % 3 == 0) || (i % 5  == 0))) {
-          const boxColorato = `<div class="box standard_color">${i}</div>`;
-          container_principale.insertAdjacentHTML('beforeend', boxColorato);
+
+     //In questa condizione il ciclo usera il contatore "i" per riempire le caselle quindi "i" sarà il contenuto del div 
+     if (!((i % 3 == 0) || (i % 5  == 0))) { 
+          const boxColorato = `<div class="box standard_color">${i}</div>`;       //dichiarazione variabile con creazione div
+          container_principale.insertAdjacentHTML('beforeend', boxColorato);      //immissione della variabile (dichiarata nella riga antecedente) nel contenitore principale   
      }
 
+
+     //In questa condizione il ciclo riempe le caselle con "fizz buzz"
      else if ((i % 3 == 0) && (i % 5  == 0)) {
-          const boxColorato = `<div class="box fizz_buzz_color">fizz buzz</div>`;
-          container_principale.insertAdjacentHTML('beforeend', boxColorato);
+          const boxColorato = `<div class="box fizz_buzz_color">fizz buzz</div>`;  //dichiarazione variabile con creazione div
+          container_principale.insertAdjacentHTML('beforeend', boxColorato);       //immissione della variabile (dichiarata nella riga antecedente) nel contenitore principale   
      }
 
+
+     //In questa condizione il ciclo riempe le caselle con "fizz"
      else if (i % 3 == 0) {
-          const boxColorato = `<div class="box fizz_color">fizz</div>`;
-          container_principale.insertAdjacentHTML('beforeend', boxColorato);   
+          const boxColorato = `<div class="box fizz_color">fizz</div>`;            //dichiarazione variabile con creazione div
+          container_principale.insertAdjacentHTML('beforeend', boxColorato);       //immissione della variabile (dichiarata nella riga antecedente) nel contenitore principale   
      }
 
+
+     //In questa condizione il ciclo riempe le caselle con "buzz"
      else if (i % 5  == 0) {
-          const boxColorato = `<div class="box buzz_color">buzz</div>`;
-          container_principale.insertAdjacentHTML('beforeend', boxColorato);
+          const boxColorato = `<div class="box buzz_color">buzz</div>`;             //dichiarazione variabile con creazione div
+          container_principale.insertAdjacentHTML('beforeend', boxColorato);        //immissione della variabile (dichiarata nella riga antecedente) nel contenitore principale   
      }
 
 }
