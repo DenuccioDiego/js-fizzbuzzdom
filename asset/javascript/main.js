1838896cd2ee5ac900b313c3bf12b01e52e87318
DenuccioDiego <diegodenuccio95@gmail.com>
2021-10-13 16:43:16 +0200

    aggiunta ulteriore condizione e risoluzione loop

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -1,37 +1,32 @@
 
 
-// let numero = 0 ;
-
-// let selezioneDiv = document.querySelector("div.container")
-// let quadrato = document.createElement("div.box") ;
-
 const sec = document.querySelector("div.container")
-
-
-
-
 
 
 for (let i = 1; i < 101; i++){
      
-     // numero = i;
-
-     // selezioneDiv.append(quadrato);
-     // quadrato.append(numero)
-     if ((i % 3 == 0) && (i % 5  == 0)){
-          console.log("ciao 5 and 3")
+     if (!((i % 3 == 0) || (i % 5  == 0))) {
+          const element = `<div class="box standard_color">${i}</div>`;
+          sec.insertAdjacentHTML('beforeend', element)
      }
 
+     else if ((i % 3 == 0) && (i % 5  == 0)){
+          const element = `<div class="box fizz_buzz_color">fizz buzz</div>`;
+          sec.insertAdjacentHTML('beforeend', element)
+          }
+
      else if (i % 3 == 0) {
-          console.log("ciao 3");
+          const element = `<div class="box fizz_color">fizz</div>`;
+          sec.insertAdjacentHTML('beforeend', element)
+          
      }
 
      else if (i % 5  == 0) {
-          console.log("ciao 5")
+          const element = `<div class="box buzz_color">buzz</div>`;
+          sec.insertAdjacentHTML('beforeend', element)
      }
 
-     const element = `<div class="box">${i}</div>`;
-     sec.insertAdjacentHTML('beforeend', element)
+    
 }
 
 
